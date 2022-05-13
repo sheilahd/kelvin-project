b3206aafb53a12f4d17f37b3610842f81d1b9ea4
sheilahd <sheila.hdez@gmail.com>
2022-05-12 20:46:47 -0400

    magic ball 2nd version

--- Magic 8 Ball/magic.js
+++ Magic 8 Ball/magic.js
@@ -1,31 +1,35 @@
-var userName = "She";
-var userQuestion = "What is your best favorite number";
-var randomNumber = Math.floor(Math.random() * 8);
-var eightBall = " ";
+const userName = "She";
+const userQuestion = "What is your best favorite number";
+const randomNumber = Math.floor(Math.random() * 8);
+let eightBall = " ";
 
-if (userName === " ") {
-  console.log("Hello");
-} else {
+if (userName) {
   console.log(`Hello ${userName}`);
-  console.log(`${userQuestion}?`);
+} else {
+  console.log("Hello");
 }
-
+console.log(`${userName} asked, ${userQuestion}`);
+console.log;
 console.log(randomNumber);
 
-eightBall = randomNumber;
+randomNumber;
 
-if (eightBall >= 0 && eightBall <= 1) {
-  console.log("It is certain");
-} else if (eightBall > 1 && eightBall <= 2) {
-  console.log("It is decidedly so");
-} else if (eightBall > 2 && eightBall <= 3) {
-  console.log("Reply hazy try again");
-} else if (eightBall > 3 && eightBall <= 4) {
-  console.log("Cannot predict now");
-} else if (eightBall > 4 && eightBall <= 5) {
-  console.log("Do not count on it");
-} else if (eightBall > 5 && eightBall <= 6) {
-  console.log("My sources say no");
-} else if (eightBall > 6 && eightBall <= 7) {
-  console.log("Outlook not so good");
-} else console.log("Signs point to yes");
+if (randomNumber === 0) {
+  eightBall = "It is certain";
+} else if (randomNumber === 1) {
+  eightBall = "It is decidedly so";
+} else if (randomNumber === 2) {
+  eightBall = "Reply hazy try again";
+} else if (randomNumber === 3) {
+  eightBall = "Cannot predict now";
+} else if (randomNumber === 4) {
+  eightBall = "Do not count on it";
+} else if (randomNumber === 5) {
+  eightBall = "My sources say no";
+} else if (randomNumber === 6) {
+  eightBall = "Outlook not so good";
+} else if (randomNumber === 7) {
+  eightBall = "Signs point to yes";
+}
+
+console.log(`Your answer is: "${eightBall}"`);
