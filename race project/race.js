163c507ea26546a655cb29d07c4a456705023a79
sheilahd <sheila.hdez@gmail.com>
2022-05-12 22:18:03 -0400

    race version 2

--- race project/race.js
+++ race project/race.js
@@ -1,21 +1,23 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 var registEarly = true;
-var runnerAge = 20;
+var runnerAge = 33;
 
 if (runnerAge > 18 && registEarly) {
   console.log(raceNumber);
 }
 
-if (runnerAge > 18 && registEarly) {
+if (runnerAge > 18 && registEarly === true) {
   console.log(
-    `You will run at 9:30 AM and your race number is: ${raceNumber + 1000}`
+    `You will run at 9:30 AM and your race number is: ${(raceNumber += 1000)}`
   );
-} else if (runnerAge > 18 && registEarly == false) {
+} else if (runnerAge > 18 && registEarly === false) {
   console.log(
     `You will run at 11:00 AM and your race number is: ${raceNumber}`
   );
-} else if (runnerAge <= 18) {
+} else if (runnerAge < 18) {
   console.log(
     `You will run at 12:30 PM and your race number is: ${raceNumber}`
   );
+} else if ((runnerAge = 18)) {
+  console.log("You have a wonderful age, you can run whenever you want..");
 }
